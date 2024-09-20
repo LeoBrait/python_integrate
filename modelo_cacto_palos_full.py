c8402454cf1d1b31f614b4c1b00584f31656483f
LeoBrait <leonardobrait@gmail.com>
2024-09-20 11:48:19 -0300

    predacao desligada

--- modelo_cacto_palos_full.py
+++ modelo_cacto_palos_full.py
@@ -4,29 +4,32 @@ from scipy.integrate import odeint
 
 # parâmetros
 r1 = 1
-r2 = 3
+r2 = 0.1
 k1 = 10
-k2 = 20
-x_inicial_1 = 1
+k2 = 1000
+x_inicial_1 = 0
 x_inicial_2 = 0
 x_inicial_3 = 100
-taxa_transicao = 1
+taxa_transicao = 0
 cacto_death = 0.1
 taxa_predacao = 0
 taxa_facilitacao = 1
 
 # passos de tempo
-t = arange(0, 1000., 0.01)
+t = arange(0, 100., 0.01)
 
 def modele_facilitacao_amensalismo(
   x, t, r1, k1, r2, k2, taxa_transicao,
   cacto_death, taxa_predacao, taxa_facilitacao):
   
   return array([
-   x[0] * (r1 + taxa_facilitacao * x[2]) - taxa_transicao * x[0],
-   taxa_transicao * x[0] - cacto_death * x[1] - k1 * x[1] ** 2,
-   r2 * x[2] * (1 - (x[2] - taxa_predacao * x[1] / k2) )
+   x[1] * (r1 + taxa_facilitacao * x[2]) - taxa_transicao * x[0],
+   taxa_transicao * x[0] - cacto_death * x[1] - k1 * (x[1] ** 2),
+   r2 * x[2] * (1 - x[2] / k2) - taxa_predacao
   ])
+
+#r2 * x[1] * ( 1 - x[1] / k2)
+
 
 modelo_integrado = odeint(modele_facilitacao_amensalismo, [x_inicial_1, x_inicial_2, x_inicial_3], t, (r1, k1, r2, k2, taxa_transicao, cacto_death, taxa_predacao, taxa_facilitacao))
 
