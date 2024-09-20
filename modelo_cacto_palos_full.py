2a1ac67ddcc4c9fd3e8b9a076039e6fdef777e43
LeoBrait <leonardobrait@gmail.com>
2024-09-20 11:06:07 -0300

    nomes modificados

--- modelo_cacto_palos_full.py
+++ modelo_cacto_palos_full.py
@@ -3,34 +3,31 @@ from matplotlib.pyplot import *
 from scipy.integrate import odeint
 
 # parâmetros
-r1 = 0.1
+r1 = 0.01
 r2 = 3
 k1 = 10
 k2 = 20
 x_inicial_1 = 0.1
 x_inicial_2 = 0.1
 x_inicial_3 = 100
-alpha = 1
-
+taxa_transicao = 0.8
 cacto_death = 0.1
-
-
-c = 0.1
-f = 10
+taxa_predacao = 0
+taxa_facilitacao = 100
 
 # passos de tempo
 t = arange(0, 10., 0.01)
 
-def modele_facilitacao_amensalismo(x, t, r1, k1, r2, k2, alpha,
+def modele_facilitacao_amensalismo(x, t, r1, k1, r2, k2, taxa_transicao,
                                    cacto_death, 
-    c, f):
+    taxa_predacao, taxa_facilitacao):
   return array([
-   x[0] * (r1 + f * x[2]) - alpha * x[0],
-   alpha * x[0] - cacto_death * x[1] - k1 * x[1] ** 2,
-   r2 * x[2] * (1 - (x[2] - c * x[1] / k2) )
+   x[0] * (r1 + taxa_facilitacao * x[2]) - taxa_transicao * x[0],
+   taxa_transicao * x[0] - cacto_death * x[1] - k1 * x[1] ** 2,
+   r2 * x[2] * (1 - (x[2] - taxa_predacao * x[1] / k2) )
  ])
 
-modelo_integrado = odeint(modele_facilitacao_amensalismo, [x_inicial_1, x_inicial_2, x_inicial_3], t, (r1, k1, r2, k2, alpha, cacto_death, c, f))
+modelo_integrado = odeint(modele_facilitacao_amensalismo, [x_inicial_1, x_inicial_2, x_inicial_3], t, (r1, k1, r2, k2, taxa_transicao, cacto_death, taxa_predacao, taxa_facilitacao))
 
 # plotando a solução
 figure(figsize=(8, 6))
