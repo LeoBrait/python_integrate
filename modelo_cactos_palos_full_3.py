7d7599cb064bd29c3d9864f4f75b2b37cd8f83aa
LeoBrait <leonardobrait@gmail.com>
2024-09-21 11:03:08 -0300

    top

--- modelo_cactos_palos_full_3.py
+++ modelo_cactos_palos_full_3.py
@@ -54,7 +54,7 @@ x_inicial_3 = 10
 # analise de pontos fixos
 
 # passos de tempo
-t = arange(0, 1000, 0.001)
+t = arange(0, 5000, 0.001)
 
 def modele_facilitacao_amensalismo(
     x, t,
@@ -94,7 +94,7 @@ modelo_oscilatorio = odeint(
 
 #############################Plotagem de interação #############################
 
-# plotando a solução do modelo estabilizado ------------------------------------
+#  estabilizado ----------------------------------------------------------------
 figure(figsize = (8, 6))
 tick_params(labelsize = 15)
 plot(t, modelo_estabilizado, linewidth = 2.5, linestyle = "-")
@@ -106,6 +106,19 @@ legend(
 )
 savefig("integrado_estavel.png")
 print(modelo_estabilizado)
+
+# Oscilatório -----------------------------------------------------------------
+figure(figsize = (8, 6))
+tick_params(labelsize = 15)
+plot(t, modelo_oscilatorio, linewidth = 2.5, linestyle = "-")
+xlabel("t", fontsize = 20) # definir rótulo do eixo x
+ylabel("x", fontsize = 20) # e do eixo y
+legend(
+  ["Cactos Recrutados (jovens)", "Cactos Adultos", "Palos Verdes"], 
+  fontsize = 16
+)
+savefig("integrado_oscilatorio.png")
+print(modelo_oscilatorio)
 
 ######################### Espaço de fase #######################################
 
@@ -134,15 +147,16 @@ savefig("espaco_de_fase_3d_estabilizado.png")
 # 2D
 figure(figsize=(10, 8))
 plot(
-  modelo_estabilizado[:, 0],
   modelo_estabilizado[:, 1],
+  modelo_estabilizado[:, 2],
   linewidth = 2.5, 
   linestyle = "-"
 )
 
 # Definindo rótulos dos eixos
-xlabel("Cactos Recrutados (jovens)", fontsize=20)
-ylabel("Cactos Adultos", fontsize=20)
+xlabel("Cactos Adultos", fontsize=20)
+ylabel("Palos Verdes", fontsize=20)
+
 
 # Salvando a figura
 savefig("espaco_de_fase_2d_estabilizado.png")
@@ -172,22 +186,22 @@ savefig("espaco_de_fase_3d_oscilatorio.png")
 # 2D
 figure(figsize=(10, 8))
 plot(
-  modelo_oscilatorio[:, 0],
   modelo_oscilatorio[:, 1],
+  modelo_oscilatorio[:, 2],
   linewidth = 2.5, 
   linestyle = "-"
 )
 
 # Definindo rótulos dos eixos
-xlabel("Cactos Recrutados (jovens)", fontsize=20)
-ylabel("Cactos Adultos", fontsize=20)
+xlabel("Cactos Adultos", fontsize=20)
+ylabel("Palos Verdes", fontsize=20)
 
 # Salvando a figura
 savefig("espaco_de_fase_2d_oscilatorio.png")
 
 ################################ Diagrama de bifurcação ########################
-figure(figsize=(10, 8))
-plot(10, modelo_integrado[-5000:, 0].min(), 'ro', markersize=10)
-plot(10, modelo_integrado[-5000:, 0].max(), 'ro', markersize=10)
-plot(10, modelo_integrado[-5000:, 1].min(), 'ob', markersize=10)
-plot(10, modelo_integrado[-5000:, 1].max(), 'ob', markersize=10)
+# figure(figsize=(10, 8))
+# plot(10, modelo_integrado[-5000:, 0].min(), 'ro', markersize=10)
+# plot(10, modelo_integrado[-5000:, 0].max(), 'ro', markersize=10)
+# plot(10, modelo_integrado[-5000:, 1].min(), 'ob', markersize=10)
+# plot(10, modelo_integrado[-5000:, 1].max(), 'ob', markersize=10)
