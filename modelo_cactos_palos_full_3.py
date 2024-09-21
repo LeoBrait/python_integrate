ad2835799da8d4690c9d9ddb3b82ad7d922d16c3
LeoBrait <leonardobrait@gmail.com>
2024-09-21 11:14:12 -0300

    top top

--- modelo_cactos_palos_full_3.py
+++ modelo_cactos_palos_full_3.py
@@ -5,23 +5,22 @@ from mpl_toolkits.mplot3d import Axes3D
 #from sympy import *
 
 
-# parâmetros
-# taxa_associacao = 3 #c
-# r_cacto_adulto = 0.006 #r1
-# taxa_facilitacao = 5 #f
-# taxa_envelhecimento = 0.02 #alpha (escala de 10^-2)
-# k_cacto_adulto = 0.009 #k1 )(inversamente proporcional)
-# morte_cacto_adulto = 0.02  #d
-# r_arbusto = 3 #r2
-# k_arbusto = 10 #k2
-# taxa_saciedade = 1 #h
-# taxa_predacao = 2 #m k 1 =< m =< 2.6
+####################### Parâmetros do modelo ###################################
+
+# taxa_associacao (c)
+# r_cacto_adulto = (r1)
+# taxa_facilitacao = (f)
+# taxa_envelhecimento = (alpha) (escala de 10^-2)
+# k_cacto_adulto = (k1) (inversamente proporcional)
+# morte_cacto_adulto = (d)
+# r_arbusto = (r2)
+# k_arbusto = (k2)
+# taxa_saciedade = (h)
+# taxa_predacao = (m) k 1 =< m =< 2.6
 
 
 parametros_controle_1 = (
-  1, 
-  0.006, 
-  5, 0.1, 0.009, 0.05, 3, 10, 1, 3
+  1, 0.006,  5, 0.1, 0.009, 0.05, 3, 10, 1, 3
 )
 
 parametros_controle_2 = (
@@ -50,12 +49,10 @@ x_inicial_2 = 0
 x_inicial_3 = 10
 
 
-
-# analise de pontos fixos
-
 # passos de tempo
 t = arange(0, 5000, 0.001)
 
+################################# Modelo #######################################
 def modele_facilitacao_amensalismo(
     x, t,
     taxa_associacao, r_cacto_adulto, 
@@ -92,7 +89,14 @@ modelo_oscilatorio = odeint(
     parametros_oscilatorio
     )
 
-#############################Plotagem de interação #############################
+modelo_teste = odeint(
+    modele_facilitacao_amensalismo,
+    [x_inicial_1, x_inicial_2, x_inicial_3],
+    t,
+    parametros_controle_1
+)
+
+#############################Plotagem de integracao ############################
 
 #  estabilizado ----------------------------------------------------------------
 figure(figsize = (8, 6))
@@ -119,6 +123,19 @@ legend(
 )
 savefig("integrado_oscilatorio.png")
 print(modelo_oscilatorio)
+
+# Teste ------------------------------------------------------------------------
+figure(figsize = (8, 6))
+tick_params(labelsize = 15)
+plot(t, modelo_teste, linewidth = 2.5, linestyle = "-")
+xlabel("t", fontsize = 20) # definir rótulo do eixo x
+ylabel("x", fontsize = 20) # e do eixo y
+legend(
+  ["Cactos Recrutados (jovens)", "Cactos Adultos", "Palos Verdes"], 
+  fontsize = 16
+)
+savefig("integrado_teste.png")
+print(modelo_teste)
 
 ######################### Espaço de fase #######################################
 
@@ -163,8 +180,7 @@ savefig("espaco_de_fase_2d_estabilizado.png")
 
 # Oscilatório ------------------------------------------------------------------
 
-# 3D
-# Plota o espaço de fase tridimensional
+# 3D -----------------------------------
 fig = figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 ax.plot(
@@ -183,7 +199,7 @@ ax.set_zlabel("Palos Verdes", fontsize=20)
 # Salvando a figura
 savefig("espaco_de_fase_3d_oscilatorio.png")
 
-# 2D
+# 2D -----------------------------------
 figure(figsize=(10, 8))
 plot(
   modelo_oscilatorio[:, 1],
@@ -198,6 +214,44 @@ ylabel("Palos Verdes", fontsize=20)
 
 # Salvando a figura
 savefig("espaco_de_fase_2d_oscilatorio.png")
+
+# Teste ------------------------------------------------------------------------
+
+# 3D -----------------------------------
+fig = figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(
+  modelo_teste[:, 0],
+  modelo_teste[:, 1],
+  modelo_teste[:, 2],
+  linewidth = 2.5, 
+  linestyle = "-"
+)
+
+# Definindo rótulos dos eixos
+ax.set_xlabel("Cactos Recrutados (jovens)", fontsize=20)
+ax.set_ylabel("Cactos Adultos", fontsize=20)
+ax.set_zlabel("Palos Verdes", fontsize=20)
+
+# Salvando a figura
+savefig("espaco_de_fase_3d_teste.png")
+
+# 2D -----------------------------------
+figure(figsize=(10, 8))
+plot(
+  modelo_teste[:, 1],
+  modelo_teste[:, 2],
+  linewidth = 2.5, 
+  linestyle = "-"
+)
+
+# Definindo rótulos dos eixos
+xlabel("Cactos Adultos", fontsize=20)
+ylabel("Palos Verdes", fontsize=20)
+
+# Salvando a figura
+savefig("espaco_de_fase_2d_teste.png")
+
 
 ################################ Diagrama de bifurcação ########################
 # figure(figsize=(10, 8))
