1e3e58b23d828e31d3b703dd3b30e424d6286373
LeoBrait <leonardobrait@gmail.com>
2024-09-20 21:08:11 -0300

    cenario com comportamento mais realista. Entretanto, passa perto do zero

--- modelo_cactos_palos_full_3.py
+++ modelo_cactos_palos_full_3.py
@@ -4,11 +4,11 @@ from scipy.integrate import odeint
 
 # parâmetros
 taxa_associacao = 2 #c
-r_cacto_adulto = 0.009 #r1
-taxa_facilitacao = 3 #f
-taxa_envelhecimento = 0.05 #alpha (escala de 10^-2)
+r_cacto_adulto = 0.006 #r1
+taxa_facilitacao = 5 #f
+taxa_envelhecimento = 0.03 #alpha (escala de 10^-2)
 k_cacto_adulto = 0.009 #k1 )(inversamente proporcional)
-morte_cacto_adulto = 0.01  #d
+morte_cacto_adulto = 0.02  #d
 r_arbusto = 3 #r2
 k_arbusto = 10 #k2
 taxa_saciedade = 1 #h
@@ -58,7 +58,7 @@ modelo_integrado = odeint(
 # plotando a solução
 figure(figsize = (8, 6))
 tick_params(labelsize = 15)
-plot(t, modelo_integrado, linewidth = 2.5, linestyle = "--")
+plot(t, modelo_integrado, linewidth = 2.5, linestyle = "-")
 xlabel("t", fontsize = 20) # definir rótulo do eixo x
 ylabel("x", fontsize = 20) # e do eixo y
 legend(["Cactos Recrutado", "Cactos Adulto", "Palos"], fontsize = 16)
